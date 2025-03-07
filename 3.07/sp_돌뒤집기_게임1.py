e9249bf7af97787877be25b11773f5ca058e2a54
이유니 <yi12128989@gmail.com>
2025-03-07 17:46:28 +0900

    파일 수정 완료

--- 3.07/sp_돌뒤집기_게임1.py
+++ 3.07/sp_돌뒤집기_게임1.py
@@ -7,10 +7,12 @@ for tc in range(1, T+1):
     arr = list(map(int, input().split()))
 
     for _ in range(M):
-        i, j = map(int, input().split())
+        i, j = map(int, input().split())    # i번째, j개 돌
 
         for k in range(i-1, i+j-1):
-            if k >= N:
+            # 인덱스라 시작은 -1로
+            # 끝나는 부분은 해당숫자 미포함이라 -1 한번 더 해줄 필요 없음 
+            if k >= N:  
                 break
             if arr[k] != arr[i-1]: 
                 arr[k] = arr[i-1]
