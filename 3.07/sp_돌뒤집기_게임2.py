df1460616aefee5623594b035e9f6115ef3cdcac
이유니 <yi12128989@gmail.com>
2025-03-07 17:34:34 +0900

    수정 완료

--- 3.07/sp_돌뒤집기_게임2.py
+++ 3.07/sp_돌뒤집기_게임2.py
@@ -8,14 +8,14 @@ for tc in range(1, T+1):
  
     for _ in range(M):
         i, j = map(int, input().split())
-        i = i - 1
+        i -= 1     # 인덱스 기준이라 -1
  
-        for k in range(1, j+1):
-            if i-k < 0 or i+k >=N:
+        for k in range(1, j+1):     # j번 반복의 필요
+            if i-k < 0 or i+k >=N:  # 범위에서 벗어나는 수 처리
                 break
      
-            if arr[i-k] == arr[i+k]:
-                arr[i-k] = (arr[i-k]+1)%2
+            if arr[i-k] == arr[i+k]:        
+                arr[i-k] = (arr[i-k]+1)%2   # 0이면 1, 1이면 0으로 바꿈
                 arr[i+k] = (arr[i+k]+1)%2
  
     print(f'#{tc}', *arr)
